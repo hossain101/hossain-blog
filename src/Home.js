a4c433dce6e38cd278de0fd24c5456c3ea79fe5c
shoaib <shoaibhossain24@gmail.com>
2023-11-03 03:46:26 +0600

    Lesson 8 : Learning userState hook.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
+
 const Home = () => {
-  return <div className="home"><h2>Home Page</h2></div>;
+
+    
+
+   const [name, setName] =  useState('shoaib');
+
+   const[age , setAge] = useState(17);
+
+    const handleClick = () => {
+        setName('shoaib hossain');
+        setAge(20);
+
+    }
+
+    //this is to show that we can call a function inside a function without invoking it by using arrow function. we can even access its event object.
+    const handleClickAgain = (name,e) => {
+        console.log('hello ' + name+ e.target);
+        console.log("Age : " + age);
+    }
+
+
+
+
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+        <p>{name}</p>
+        <p>age: {age}</p>
+      <button onClick={handleClick}>Click</button>
+        <button onClick={(e) => {
+            handleClickAgain('shoaib',e);
+        }}>Click Again</button>
+    </div>
+  );
+
+
+
 };
 
 export default Home;
