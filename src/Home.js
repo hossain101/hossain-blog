1ef6f237c410f5c7592f51679f28f57f9f2082cd
shoaib <shoaibhossain24@gmail.com>
2023-11-03 09:59:46 +0600

    tutorial 10: addi boilerplate array, map them and display them. Learn about the importance of key.

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,38 @@
 import { useState } from "react";
 
-
 const Home = () => {
-
-    
-
-   const [name, setName] =  useState('shoaib');
-
-   const[age , setAge] = useState(17);
-
-    const handleClick = () => {
-        setName('shoaib hossain');
-        setAge(20);
-
-    }
-
-    //this is to show that we can call a function inside a function without invoking it by using arrow function. we can even access its event object.
-    const handleClickAgain = (name,e) => {
-        console.log('hello ' + name+ e.target);
-        console.log("Age : " + age);
-    }
-
-
-
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "shoaib",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "hossain",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "sabit",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
       <h2>Home Page</h2>
-        <p>{name}</p>
-        <p>age: {age}</p>
-      <button onClick={handleClick}>Click</button>
-        <button onClick={(e) => {
-            handleClickAgain('shoaib',e);
-        }}>Click Again</button>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            </div>
+        ))}
     </div>
   );
-
-
-
 };
 
 export default Home;
