a8054fea27af197bf25aace7f7fe8966adc2c478
shoaib <shoaibhossain24@gmail.com>
2023-11-08 08:41:47 +0600

    Refactor code to use destructuring and add abort controller for fetch requests

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,19 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  
-  const {data:blogs,isPending,error} = useFetch('http://localhost:8000/blogs');
-  
-
-
-
-  
-
-
-  
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      
       <h2>Home Page</h2>
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-     
-      
     </div>
   );
 };
--- src/useFetch.js
+++ src/useFetch.js
@@ -7,7 +7,11 @@ const useFetch = (url) => {
     
     
     useEffect(() => {
-        fetch(url)
+
+        const abortCont = new AbortController();
+
+
+        fetch(url, {signal: abortCont.signal})
             .then((res) => {
                 if(!res.ok){
                     throw Error('could not fetch the data for that resource');
@@ -20,10 +24,16 @@ const useFetch = (url) => {
                 setIsPending(false);
                 setError(null);
             }).catch((err) => {
+                if(err.name === 'AbortError'){
+                    console.log('fetch aborted');
+                }
+                else{
                 setError(err.message);
                 setIsPending(false);
                 console.log(err.message);
+                }
             })
+            return () => abortCont.abort();
     }, [url]);
 
     return {data, isPending, error};
