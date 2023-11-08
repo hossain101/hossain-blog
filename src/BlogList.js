1a9805a8a7183663f679315647ab34e91d0c0227
shoaib <shoaibhossain24@gmail.com>
2023-11-08 08:07:42 +0600

    Remove handleDelete function and useFetch hook in BlogList and Home components

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,7 @@ const BlogList = (props) => {
 
     const blogs = props.blogs;
     const title = props.title;
-    const handleDelete = props.handleDelete;
+    
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -11,7 +11,7 @@ const BlogList = (props) => {
             <div className="blog-preview" key={blog.id}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
-            <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
+           
             </div>
         ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,18 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
+  
+  const {data:blogs,isPending,error} = useFetch('http://localhost:8000/blogs');
+  
 
- 
 
-  const[isPending, setIsPending] = useState(true);
 
-  const[error, setError] = useState(null);
+  
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
 
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if(!res.ok){
-          throw Error('could not fetch the data for that resource');
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      }).catch((err) => {
-        setError(err.message);
-        setIsPending(false);
-        console.log(err.message);
-      })
-  }, []);
+  
 
   return (
     <div className="home">
@@ -41,9 +20,7 @@ const Home = () => {
       <h2>Home Page</h2>
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && (
-        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      )}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
      
       
     </div>
