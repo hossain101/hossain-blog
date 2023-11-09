dba5ef0778d2ab51fa81eac288807e8551771caa
shoaib <shoaibhossain24@gmail.com>
2023-11-09 15:00:54 +0600

    Add BlogDetails component and link to individual blog pages

---
+++ src/BlogDetails.js
@@ -0,0 +1,28 @@
+
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+
+    const {id } = useParams();//useParams is a hook that allows us to access the route parameters from the url, we get the id from the url
+    const {data : blog,error,isPending} = useFetch('http://localhost:8000/blogs/' + id);
+
+
+    return ( 
+        <div className="blog-details">
+
+           {isPending && <div>Loading...</div>}
+           {error && <div>{error}</div>}
+              {blog && (
+                    <article>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                        <div>{blog.body}</div>
+                    </article>
+                )}
+        </div>
+        
+     );
+}
+ 
+export default BlogDetails;
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = (props) => {
 
     const blogs = props.blogs;
@@ -9,8 +11,10 @@ const BlogList = (props) => {
 
             {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
+                <Link to={`/blogs/${blog.id}`}>
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
+            </Link>
            
             </div>
         ))}
