a1ce1db7c94f9ea4ee5c2962f5460f7499aee1bb
shoaib <shoaibhossain24@gmail.com>
2023-11-11 15:40:31 +0600

    Refactor Create.js to use async/await for fetching data

--- src/Create.js
+++ src/Create.js
@@ -1,108 +1,52 @@
-
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-
-
-const Create =  () => {
-    const [title,setTitle] = useState('');
-    const [body,setBody] = useState('');
-    const [author,setAuthor] = useState('');
-    const[isPending,setIsPending] = useState(false);
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState(false);
     const history = useHistory();
-    let lastBlogId = 0;
 
-// const getLastBlogId = () => {
-//     fetch('http://localhost:8000/blogs')
-//     .then((res) => {
-//         if(!res.ok){
-//             throw Error('could not fetch the data for that resource');
-//         }
-//         return res.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//         return data.length;
-//     }).catch((err) => {
-//         console.log(err.message);
-//     })
-// }
+    const getLastBlogId = async () => {
+        const response = await fetch('http://localhost:8000/blogs');
+        const data = await response.json();
+        return data.length;
+    }
 
-const getLastBlogId2 = async () => {
-    const response = await fetch('http://localhost:8000/blogs');
-    const data = await response.json();
-    console.log(data.length);
-    return data.length;
-    
-}
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
 
+        setIsPending(true);
 
- getLastBlogId2().then((data) => {
-    lastBlogId = data;
-    console.log(lastBlogId);
-}).catch((err) => {
-    console.log(err.message);
-});
+        await fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        });
 
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    const blog = {title,body,author};
-    console.log(blog);
-
-    setIsPending(true);
-
-   await fetch('http://localhost:8000/blogs',{
-        method: 'POST',
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(blog)
-    }).then(() => {
-        console.log('new blog added');
-        console.log(lastBlogId);
-       
         setIsPending(false);
-       
 
-        //Move to the latest BlogDetails component
-       // history.push('/blogs/' + getLastBlogId2());
-    })
-    .catch((err) => {
-        console.log(err.message);
-    }   
-    ).then(() => {
-        console.log(lastBlogId);
-        console.log('sending to new page');
-        
-        history.push(`blogs/${lastBlogId+1}`);
-    });
-    //Move to the latest BlogDetails component
-    
+        const lastBlogId = await getLastBlogId();
+        history.push(`blogs/${lastBlogId }`);
+    }
 
-    // history.go(-1);//go back to the previous page
-}
-
-
-
-    return ( 
+    return (
         <div className="create">
             <h2>Add a New Blog</h2>
             <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
-                <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
-                {/* In the onChange we get back an annonymous function and this function has properties we can access. */}
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
                 <label>Blog body:</label>
-                <textarea required value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
                 <label>Blog author:</label>
-              <input type="text" required value = {author} onChange={(e)=>setAuthor(e.target.value)} />
-               { !isPending&&<button>Add Blog</button>}
-               { isPending&&<button disabled>Adding Blog...</button>}
-
-
-                {/* <p>{title}</p>
-                <p>{body}</p> */}
-
+                <input type="text" required value={author} onChange={(e) => setAuthor(e.target.value)} />
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
             </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
